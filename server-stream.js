89cac3f8ce2e3029dcafa9b87e246c9b61be0261
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2025-01-10 22:48:47 -0800

    unique numbers

--- server-stream.js
+++ server-stream.js
@@ -22,25 +22,40 @@ const server = http.createServer((req, res) => {
 
   res.setHeader('Content-Type', 'application/json');
 
-  const generateNumber = () => {
-    if (numberType === 'float') {
-      const min = Math.pow(10, numLen - 1); 
-      const max = Math.pow(10, numLen) - 1; 
-      return Math.random() * (max - min) + min; 
-    } else if (numberType === 'integer') {
-      const min = Math.pow(10, numLen - 1); 
-      const max = Math.pow(10, numLen) - 1;
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  };
 
-  const numbers = [];
-  for (let i = 0; i < qty; i++) {
-    numbers.push(generateNumber());
-  }
+  // const numbers = [];
+  // for (let i = 0; i < qty; i++) {
+  //   numbers.push(generateNumber());
+  // }
+  const numbers = generateUniqueElArray(numLen, numberType, qty)
 
   res.end(JSON.stringify(numbers));
 });
+
+// add to assistant-js (from kivi)
+// fns generateRandomName or .. pass to args
+function generateUniqueElArray(numLen, numType, count) {
+    const uniqueNames = new Set();
+
+    while (uniqueNames.size < count) {
+      const name = generateNumber(numLen, numType);
+      uniqueNames.add(name);
+    }
+    return Array.from(uniqueNames);
+}
+
+function generateNumber(intLen, numType) {
+  if (numType === 'float') {
+    const min = Math.pow(10, intLen - 1); 
+    const max = Math.pow(10, intLen) - 1; 
+    return Math.random() * (max - min) + min; 
+  } else if (numType === 'integer') {
+    const min = Math.pow(10, intLen - 1); 
+    const max = Math.pow(10, intLen) - 1;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+};
+
 
 const port = 3000;
 server.listen(port, () => {
